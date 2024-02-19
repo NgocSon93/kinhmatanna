0a8b9af0f75a7751d26059df006a9bbcdba8dc81
NgocSon93 <Dangngocson931@gmail.com>
2024-02-19 22:24:01 +0700

    add responsive header

--- src/NavBar/Navbar-Header.vue
+++ src/NavBar/Navbar-Header.vue
@@ -3,27 +3,45 @@
     <div class="header-logo">
       <img src="../assets/logo-anna.png" />
     </div>
-    <div class="header-nav">
-      <router-link :to="{ name: 'home' }">TRANG CHỦ</router-link>
-      <router-link :to="{ name: 'hanhtrinhtute' }"
+    <div class="header_nav-pc">
+      <router-link class="header_nav-pc--link" :to="{ name: 'home' }">TRANG CHỦ</router-link>
+      <router-link class="header_nav-pc--link" :to="{ name: 'hanhtrinhtute' }"
         >HÀNH TRÌNH TỬ TẾ</router-link
       >
-      <router-link :to="{ name: 'sanpham' }">SẢN PHẨM</router-link>
-      <router-link :to="{ name: 'veanna' }">VỀ ANNA</router-link>
-      <router-link :to="{ name: 'blog' }">BLOG</router-link>
-      <router-link :to="{ name: 'oderchecking' }">ODER CHECKING</router-link>
-      <router-link :to="{ name: 'store' }">STORE</router-link>
+      <router-link class="header_nav-pc--link" :to="{ name: 'sanpham' }">SẢN PHẨM</router-link>
+      <router-link class="header_nav-pc--link" :to="{ name: 'veanna' }">VỀ ANNA</router-link>
+      <router-link class="header_nav-pc--link" :to="{ name: 'blog' }">BLOG</router-link>
+      <router-link class="header_nav-pc--link" :to="{ name: 'oderchecking' }">ODER CHECKING</router-link>
+      <router-link class="header_nav-pc--link" :to="{ name: 'store' }">STORE</router-link>
+    </div>
+    <label for="nav-mobile-input" class="header_nav-icon">
+      <font-awesome-icon :icon="['fas', 'bars']" />
+    </label>
+    <input type="checkbox" hidden class="nav_input" id="nav-mobile-input">
+    <label for="nav-mobile-input" class="header_nav-overlay"></label>
+    <div class="header_nav-mobile">
+      <label for="nav-mobile-input" class="header_nav-mobile--close">
+        <font-awesome-icon :icon="['fas', 'xmark']" />
+      </label>
+      <router-link class="header_nav-mobile--link" :to="{ name: 'home' }">TRANG CHỦ</router-link>
+      <router-link class="header_nav-mobile--link" :to="{ name: 'hanhtrinhtute' }"
+        >HÀNH TRÌNH TỬ TẾ</router-link
+      >
+      <router-link class="header_nav-mobile--link" :to="{ name: 'sanpham' }">SẢN PHẨM</router-link>
+      <router-link class="header_nav-mobile--link" :to="{ name: 'veanna' }">VỀ ANNA</router-link>
+      <router-link class="header_nav-mobile--link" :to="{ name: 'blog' }">BLOG</router-link>
+      <router-link class="header_nav-mobile--link" :to="{ name: 'oderchecking' }">ODER CHECKING</router-link>
+      <router-link class="header_nav-mobile--link" :to="{ name: 'store' }">STORE</router-link>
     </div>
     <div class="header-icon">
-      <button type="button" class="btn position-relative">
+      <button type="button" class="btn position-relative header-icon-search">
         <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
       </button>
-      <button type="button" class="btn position-relative">
+      <button type="button" class="btn position-relative header-icon-user">
         <font-awesome-icon :icon="['far', 'circle-user']" />
       </button>
-      <button type="button" class="btn position-relative">
-        <router-link
-          class="nav-link"
+      <button type="button" class="btn position-relative header-icon-cart">
+        <router-link class="nav-link"
           :class="$route.name == 'Cart' ? 'active' : ''"
           aria-current="page"
           :to="{ name: 'Cart' }"
@@ -41,7 +59,7 @@
   </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 * {
   margin: 0;
   padding: 0;
@@ -52,7 +70,7 @@
   left: 0;
   width: 100%;
   padding: 20px 100px;
-  background: rgba(255, 255, 255, 0.1);
+  background: rgba(0, 0, 0, 0.1);
   display: flex;
   align-items: center;
   justify-content: space-around;
@@ -60,7 +78,7 @@
 .header-logo img {
   height: 60px;
 }
-.header-nav a {
+.header_nav-pc--link {
   font-family: "Montserrat", Sans-serif;
   font-size: 16px;
   font-weight: 700;
@@ -71,7 +89,10 @@
   margin-left: 35px;
   transition: 0.3s;
 }
-.header-nav a:hover {
+.nav_link {
+  margin-left: 0 !important;
+}
+.header_nav-pc--link:hover {
   color: #81c8c2;
 }
 .header a.router-link-exact-active {
@@ -81,9 +102,73 @@
   font-size: 25px;
   margin: 0 10px;
 }
+.header_nav-icon  {
+  font-size: 25px;
+  margin: 0 10px;
+  display: none;
+}
+.header_nav-mobile--close {
+  font-size: 25px;
+  cursor: pointer;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+}
 .header-icon button span {
   font-size: 15px;
   padding: 3px;
   color: #ffffff;
 }
+.header_nav-overlay {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: none;
+  animation: fadeIn linear 0.2s;
+  z-index: 1;
+}
+
+.header_nav-mobile {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 320px;
+  max-width: 100%;
+  background-color: #fff;
+  transform: translateX(-100%);
+  opacity: 0;
+  transition: transform linear 0.2s,opacity linear 0.2s;
+  z-index: 2;
+}
+.header_nav-mobile--link {
+  text-decoration: none;
+  color: #333;
+  display: block;
+  padding: 8px 15px;
+  font-size: 1.1rem;
+  font-weight: 500;
+}
+
+.nav_input:checked ~ .header_nav-overlay {
+  display: block;
+}
+
+.nav_input:checked ~ .header_nav-mobile {
+  transform: translateX(0);
+  opacity: 1;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+@import "./header.scss"
 </style>
